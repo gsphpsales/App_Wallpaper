742f669cdb2adc9d3e1519c71de168fccc903a8a
Gilson Sales <gilson.sales21@hotmail.com>
2019-07-04 13:43:45 -0300

    Dont use favority
    But  was adding call of the video premium

--- app/src/main/java/com/bytecod/wallpaperhd/adapters/WallpaperAdapter.java
+++ app/src/main/java/com/bytecod/wallpaperhd/adapters/WallpaperAdapter.java
@@ -20,7 +20,6 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.Toast;
-
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
 import com.bumptech.glide.request.target.SimpleTarget;
@@ -29,10 +28,14 @@ import com.bytecod.wallpaperhd.BuildConfig;
 import com.bytecod.wallpaperhd.R;
 import com.bytecod.wallpaperhd.models.Wallpaper;
 import com.github.clans.fab.FloatingActionButton;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.reward.RewardItem;
+import com.google.android.gms.ads.reward.RewardedVideoAd;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -42,14 +45,18 @@ import java.util.List;
 import jp.wasabeef.glide.transformations.BlurTransformation;
 
 public class WallpaperAdapter extends RecyclerView.Adapter<WallpaperAdapter.WallpapersViewHolder> {
-
+    private InterstitialAd mInterstitialAd;
     private Context mCtx;
     private List<Wallpaper> wallpaperList;
+    private RewardedVideoAd mAd;
     //private String category;
     public WallpaperAdapter(Context mCtx, List<Wallpaper> wallpaperList) {
         this.mCtx = mCtx;
         this.wallpaperList = wallpaperList;
       //  this.category = category;
+        mInterstitialAd = new InterstitialAd(mCtx);
+        mInterstitialAd.setAdUnitId("ca-app-pub-1231054257159317/5896639723");
+        mInterstitialAd.loadAd(new AdRequest.Builder().build());
     }
     @Override
     public WallpapersViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -68,42 +75,39 @@ public class WallpaperAdapter extends RecyclerView.Adapter<WallpaperAdapter.Wall
         Glide.with(mCtx)
                 .load(w.url)
                 .into(holder.imageView);
-        if (w.isFavourite){
-            holder.checkBoxFav.setChecked(true);
-        }
-    }
 
+    }
     @Override
     public int getItemCount() {
         return wallpaperList.size();
     }
-
     class WallpapersViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener,
             CompoundButton.OnCheckedChangeListener {
-
         //TextView textView;
         ImageView imageView;
         ImageView imageViewBlur;
-        FloatingActionButton fab2, fab1;
-        CheckBox checkBoxFav;
-        //ImageButton buttonShare, button_set;
+        FloatingActionButton fab2, fab1, fab3;
 
+        //ImageButton buttonShare, button_set;
 
         public WallpapersViewHolder(View itemView) {
             super(itemView);
             //textView = itemView.findViewById(R.id.text_view_title);
             imageView = itemView.findViewById(R.id.imageview_reclycler);
             imageViewBlur = itemView.findViewById(R.id.imageview_reclycler_blur);
-            checkBoxFav = itemView.findViewById(R.id.checkbox_favorite);
+
             //buttonShare =  itemView.findViewById(R.id.button_share);
             fab1 = itemView.findViewById(R.id.material_design_floating_action_menu_item2);
             fab2 = itemView.findViewById(R.id.material_design_floating_action_menu_item3);
+    //        fab3 = itemView.findViewById(R.id.material_design_floating_action_menu_item4);
             //button_set = itemView.findViewById(R.id.button_set);
 
-            checkBoxFav.setOnCheckedChangeListener(this);
+
             //buttonShare.setOnClickListener(this);
             fab1.setOnClickListener(this);
             fab2.setOnClickListener(this);
+  //          fab3.setOnClickListener(this);
+            mInterstitialAd.isLoaded();
             //button_set.setOnClickListener(this);
 
             //imageView.setImageBitmap();
@@ -122,6 +126,11 @@ public class WallpaperAdapter extends RecyclerView.Adapter<WallpaperAdapter.Wall
                 case R.id.material_design_floating_action_menu_item3:
                     setWallpaper(wallpaperList.get(getAdapterPosition()));
                     break;
+
+/*
+                case R.id.material_design_floating_action_menu_item4:
+                    downloadWallpaper(wallpaperList.get(getAdapterPosition()));
+                    break;*/
             }
 
         }
@@ -141,8 +150,8 @@ public class WallpaperAdapter extends RecyclerView.Adapter<WallpaperAdapter.Wall
 
                                   try {
                                       manager.setBitmap(resource);
-                                      Toast.makeText(mCtx, "Wallpaper salvo com sucesso", Toast.LENGTH_LONG).show();
-
+                                      Toast.makeText(mCtx, "Wallpaper salvo com sucesso, agora uma propraganda para manter o app grátis.", Toast.LENGTH_LONG).show();
+                                      mInterstitialAd.show();
                                   }catch (IOException e){
                                       Toast.makeText(mCtx,"Error", Toast.LENGTH_SHORT).show();
                                   }
@@ -164,17 +173,14 @@ public class WallpaperAdapter extends RecyclerView.Adapter<WallpaperAdapter.Wall
                               @Override
                               public void onResourceReady(Bitmap resource, Transition<? super Bitmap> transition) {
                                   ((Activity) mCtx).findViewById(R.id.progressbar).setVisibility(View.GONE);
-
                                   Intent intent = new Intent(Intent.ACTION_SEND);
                                   intent.setType("images/*");
                                   intent.putExtra(Intent.EXTRA_STREAM, getLocalBitmapUri(resource));
-
                                   mCtx.startActivity(Intent.createChooser(intent, "wallpaper_hub"));
                               }
                           }
                     );
         }
-
         private Uri getLocalBitmapUri(Bitmap bmp) {
             Uri bmpUri = null;
             try {
@@ -191,64 +197,49 @@ public class WallpaperAdapter extends RecyclerView.Adapter<WallpaperAdapter.Wall
             }
             return bmpUri;
         }
-
-
         private void downloadWallpaper(final Wallpaper wallpaper) {
             ((Activity) mCtx).findViewById(R.id.progressbar).setVisibility(View.VISIBLE);
-
             Glide.with(mCtx)
                     .asBitmap()
-                    .load(wallpaper.urlSource)
+                    .load(wallpaper.url)
                     .into(new SimpleTarget<Bitmap>() {
                               @Override
                               public void onResourceReady(Bitmap resource, Transition<? super Bitmap> transition) {
                                   ((Activity) mCtx).findViewById(R.id.progressbar).setVisibility(View.GONE);
-
                                   Intent intent = new Intent(Intent.ACTION_VIEW);
-
                                   Uri uri = saveWallpaperAndGetUri(resource, wallpaper.id);
-
                                   if (uri != null) {
                                       intent.setDataAndType(uri, "images/*");
-                                      mCtx.startActivity(Intent.createChooser(intent, "wallpaper_hub"));
+                                      mCtx.startActivity(Intent.createChooser(intent, "wallpaper hub"));
                                   }
                               }
                           }
                     );
         }
-
-
         private Uri saveWallpaperAndGetUri(Bitmap bitmap, String id) {
             if (ContextCompat.checkSelfPermission(mCtx, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
                     != PackageManager.PERMISSION_GRANTED) {
-
                 if (ActivityCompat
                         .shouldShowRequestPermissionRationale((Activity) mCtx, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-
                     Intent intent = new Intent();
                     intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-
                     Uri uri = Uri.fromParts("package", mCtx.getPackageName(), null);
                     intent.setData(uri);
-
                     mCtx.startActivity(intent);
-
                 } else {
                     ActivityCompat.requestPermissions((Activity) mCtx, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, 100);
                 }
                 return null;
             }
-
-            File folder = new File(Environment.getExternalStorageDirectory().toString() + "/wallpaper_hub");
+            File folder = new File(Environment.getExternalStorageDirectory().toString() + "/Series_Wallpaper");
             folder.mkdirs();
-
             File file = new File(folder, id + ".jpg");
             try {
                 FileOutputStream out = new FileOutputStream(file);
                 bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
                 out.flush();
                 out.close();
-
+                //return Uri.fromFile(file);
                 return FileProvider.getUriForFile(mCtx, BuildConfig.APPLICATION_ID + ".provider", file);
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
@@ -261,27 +252,18 @@ public class WallpaperAdapter extends RecyclerView.Adapter<WallpaperAdapter.Wall
         @Override
         public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
 
-            if (FirebaseAuth.getInstance().getCurrentUser() == null) {
+           /* if (FirebaseAuth.getInstance().getCurrentUser() == null) {
                 Toast.makeText(mCtx, "Faça o login primeiro...", Toast.LENGTH_LONG).show();
                 compoundButton.setChecked(false);
                 return;
-            }
+            }*/
 
 
             int position = getAdapterPosition();
             Wallpaper w = wallpaperList.get(position);
 
 
-            DatabaseReference dbFavs = FirebaseDatabase.getInstance().getReference("users")
-                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
-                    .child("favourites")
-                    .child(w.category);
 
-            if (b) {
-                dbFavs.child(w.id).setValue(w);
-            } else {
-                dbFavs.child(w.id).setValue(null);
-            }
         }
     }
 }
